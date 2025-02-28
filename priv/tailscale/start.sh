f2772f08d31fde0039e76133d3e94fd479f7642e
Garrett Julaka <garrett@cuttime.net>
2025-02-28 11:42:54 -0600

    Server isn't keeping tailscale alive

--- priv/tailscale/start.sh
+++ priv/tailscale/start.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 /usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-
-BACK_PID=$!
-
-while kill -0 $BACK_PID ; do
-    echo "Process is still active..."
-    sleep 1
-done
-
-
 /usr/bin/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=fly-app
-/app/bin/server
+tail -f /dev/null
+
+
+# /app/bin/server
+#
+# /usr/sbin/tailscaled
