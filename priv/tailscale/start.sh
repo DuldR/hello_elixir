15816df7f45a516e721a733c7fea2379708b25c6
Garrett Julaka <garrett@cuttime.net>
2025-02-28 11:39:15 -0600

    Try a while loop

--- priv/tailscale/start.sh
+++ priv/tailscale/start.sh
@@ -3,7 +3,12 @@
 /usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 
 BACK_PID=$!
-wait $BACK_PID
+
+while kill -0 $BACK_PID ; do
+    echo "Process is still active..."
+    sleep 1
+done
+
 
 /usr/bin/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=fly-app
 /app/bin/server
