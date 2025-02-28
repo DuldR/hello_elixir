3268ad1fac35da98f2d69a58942397bf2b925bbe
Garrett Julaka <garrett@cuttime.net>
2025-02-28 11:36:26 -0600

    Wait for the daemon to come up

--- priv/tailscale/start.sh
+++ priv/tailscale/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 /usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
+
+BACK_PID=$!
+wait $BACK_PID
+
 /usr/bin/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=fly-app
 /app/bin/server
